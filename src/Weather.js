d4e56a98f03c342b58f9980c8174ebeaa7689d0a
169233 <benedikta.jana@seznam.cz>
2023-05-21 13:47:57 +0200

    Change color

---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Weather.css"
+import axios from "axios";
+import Loader from "react-loader-spinner";
+export default function Weather(){
+    return(
+        <h2>"Hello from Weather"</h2>
+    )
+}
